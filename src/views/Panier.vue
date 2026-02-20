8dde93db58b58392feba2ad3cf3ff5a490fb4138
Musrh <musplay2023@gmail.com>
2026-02-20 20:39:37 +0000

    Update Panier.vue

--- src/views/Panier.vue
+++ src/views/Panier.vue
@@ -1,14 +1,34 @@
-
 <template>
-  <button @click="testClick">Payer avec Stripe</button>
+  <button @click="checkout">Payer avec Stripe</button>
 </template>
 
 <script>
+import { loadStripe } from '@stripe/stripe-js';
+
 export default {
   methods: {
-    testClick() {
-      alert("Bouton fonctionne !");
-      console.log("Click OK");
+    async checkout() {
+      console.log("Click détecté");
+
+      const stripe = await loadStripe(
+        'pk_test_51T20K6AwgHqDmd0F8LcnioXKpuzSyQv7aPkDhhmtPEH9BA98KOzf6F43K2O4A5WjhHHVlguyp48W0bmqMbwSvcDm00YINXIME3'
+      );
+
+      console.log("Stripe chargé");
+
+      const response = await fetch(
+        'https://stripe-backend-production-2ac4.up.railway.app/create-checkout-session',
+        { method: 'POST' }
+      );
+
+      console.log("Réponse reçue");
+
+      const session = await response.json();
+      console.log(session);
+
+      await stripe.redirectToCheckout({
+        sessionId: session.id,
+      });
     }
   }
 }
