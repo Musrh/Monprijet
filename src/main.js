12b1c9df86eae6125289f192510e6a3d94e4a5d5
Musrh <musplay2023@gmail.com>
2026-02-21 17:40:59 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,22 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
-
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
-
-// Init Vuex auth state
-store.dispatch("initAuth");
-
-// Redirection si non connecté
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    router.push("/");
-  }
-});
 
 app.mount("#app");
