8b2adc6226c526d29d6ac215a25f2bd440b6e5df
Musrh <musplay2023@gmail.com>
2026-02-22 07:14:18 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,66 @@
 <template>
-<div>
-<nav>
-<router-link to="/">Home</router-link> |
-<router-link to="/contact">Contact</router-link> |
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/contact">Contact</router-link> |
+      <router-link to="/produits">Produits</router-link> |
 
-<router-link to="/produits">Produits</router-link>|
+      <!-- Admin panel -->
+      <router-link v-if="isAdmin" to="/admin">
+        Admin
+      </router-link> |
 
-<router-link v-if="isAdmin" to="/admin">
-    Admin
-    </router-link>
+      <!-- Login -->
+      <router-link v-if="!isAuthenticated" to="/login">
+        Login
+      </router-link> |
 
-<router-link v-if="!isAuthenticated" to="/login">
-  Login
-</router-link>
+      <!-- Panier -->
+      <router-link to="/panier">
+        🛒 ({{ cartItemCount }})
+      </router-link> |
 
-<router-link to="/panier">
-    🛒 ({{ $store.getters.cartItemCount }})
-    </router-link>
+      <!-- Admin commandes -->
+      <router-link v-if="isAdmin" to="/admin-commandes">
+        Admin Commandes
+      </router-link>
 
-  <router-link v-if="$store.getters.isAdmin" to="/admin-commandes">
-          Admin Commandes
-              </router-link>
+      <!-- User info -->
+      <span v-if="isAuthenticated">
+        | {{ userEmail }}
+        <button @click="logout">Logout</button>
+      </span>
+    </nav>
 
-<span v-if="isAuthenticated">
-  | {{ userEmail }}
-  <button @click="logout">Logout</button>
-</span>
-
-
-</nav>
-
-  <router-view />
-</div>
+    <router-view />
+  </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 
 export default {
-computed: {
-  ...mapGetters(["isAuthenticated", "userEmail"]),
+  computed: {
+    ...mapGetters([
+      "isAuthenticated",
+      "userEmail",
+      "isAdmin",
+      "cartItemCount"
+    ])
+  },
 
-  ...mapGetters(["isAuthenticated", "userEmail", "isAdmin"])
-},
-methods: {
-  logout() {
-  this.$store.dispatch("logout");
-  this.$router.dispatch("/");
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+      this.$router.push("/"); // ✅ CORRECT
+    }
   }
-}
 };
 </script>
 
 <style>
-
 nav {
-    margin-left: 0px;
-      font-weight: bold;
-        color: green;
-        }
-
-
-
+  font-weight: bold;
+  color: green;
+}
 </style>
