8b2de0af8838289936bb3150805f3b2f126fd0f1
Musrh <musplay2023@gmail.com>
2026-02-24 07:22:42 +0000

    Update Success.vue

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -1,44 +1,20 @@
 <template>
-<div class="success-page">
-  <h2>🎉 Paiement réussi !</h2>
-  <p>Merci pour votre commande. Votre paiement a été accepté.</p>
-  <router-link to="/panier">
-    Retour au panier
-  </router-link>
-</div>
+  <div>
+    <h1>Paiement réussi !</h1>
+    <p>Merci pour votre commande.</p>
+    <p v-if="orderId">ID de la commande : {{ orderId }}</p>
+  </div>
 </template>
 
 <script>
+import { useRoute } from "vue-router";
+
 export default {
-name: "Success"
-}
+  setup() {
+    const route = useRoute();
+    const orderId = route.query.orderId || null;
+
+    return { orderId };
+  }
+};
 </script>
-
-<style scoped>
-.success-page {
-text-align: center;
-margin-top: 50px;
-}
-
-.success-page h2 {
-color: green;
-}
-
-.success-page p {
-margin: 15px 0;
-}
-
-.success-page a {
-display: inline-block;
-margin-top: 20px;
-padding: 10px 20px;
-background-color: #42b983;
-color: white;
-border-radius: 6px;
-text-decoration: none;
-}
-
-.success-page a:hover {
-background-color: #369870;
-}
-</style>
