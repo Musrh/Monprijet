99b4303ec6f06585db9f7919204a1a7db76b4933
Musrh <musplay2023@gmail.com>
2026-02-24 02:48:00 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,11 +5,9 @@ import store from "./store";
 
 const app = createApp(App);
 
-// 🔥 Important : initialise Vuex et le router
-app.use(store);
-app.use(router);
-
-// 🔥 Initialisation de l'auth Firebase avant rendu du menu
+// 🔹 Initialiser l'utilisateur avant de monter l'app
 store.dispatch("initAuth").then(() => {
+  app.use(store);
+  app.use(router);
   app.mount("#app");
 });
