114537f223c6e1bf55b360cdcce6932d875649e0
Musrh <musplay2023@gmail.com>
2026-02-25 04:31:16 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // 🔹 Important : base doit correspondre au nom de ton dépôt GitHub
 export default defineConfig({
   plugins: [vue()],
-  base: '/monprijet/'  // ✅ Change 'Monprijet' par le nom exact de ton dépôt
+  base: '/Monprijet/'  // ✅ Change 'Monprijet' par le nom exact de ton dépôt
 })
