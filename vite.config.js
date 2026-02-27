f70c25500a7eb52a856b0b1a16a5cf57504303ca
Musrh <musplay2023@gmail.com>
2026-02-27 06:49:07 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Monprijet/',  // ✅ Nom exact de ton dépôt GitHub
-  define: {
-    'process.env': {}   // pour certains packages qui lisent process.env
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: { global: 'globalThis' },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true
-        })
-      ]
-    }
-  }
+  base: '/Monprijet/'  // Nom exact du dépôt
 })
