5062b252721754227e23c1d47edf0d6350814e53
Musrh <musplay2023@gmail.com>
2026-02-27 07:13:35 +0000

    Update Success.vue

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Paiement réussi ! ✅</h1>
-    <p>Merci pour votre commande.</p>
+  <div class="p-4">
+    <h1 class="text-2xl mb-4 text-green-600">Paiement réussi ✅</h1>
+    <p>Merci pour votre commande !</p>
   </div>
 </template>
