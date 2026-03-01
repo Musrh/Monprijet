8cee428338de7fe383866b413cec2974e701a611
Musrh <musplay2023@gmail.com>
2026-03-01 06:29:59 +0000

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,7 +53,7 @@
 <script>
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
-import SliderProducts from "../components/SliderProducts.vue";
+import SliderProducts from "./SliderProducts.vue";
 
 export default {
   components: { SliderProducts },
