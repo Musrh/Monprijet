cc8ef513d617cc3583951c7ebc525422f59eb3b1
Musrh <musplay2023@gmail.com>
2026-03-02 17:20:28 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 // CSS global thème
-//import "./style.css";
+import "./style.css";
 
 // Import du thème
 import './themes/dark.css';
