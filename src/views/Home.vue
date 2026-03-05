fd7f084e165e13c7e6738c63f2e948caa1523e84
Musrh <musplay2023@gmail.com>
2026-03-05 17:24:32 +0000

    Update Home.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <!-- Slider des produits en promo -->
-    <SliderProducts :produits="produits" />
+  <div class="px-4">
+
+    <!-- Section Slider + Pub -->
+    <section class="flex flex-col md:flex-row gap-4 my-8">
+
+      <!-- Slider des produits promos -->
+      <div class="w-full md:w-3/4">
+        <SliderProducts :produits="produitsPromos" />
+      </div>
+
+      <!-- Pub à côté du slider -->
+      <div class="w-full md:w-1/4">
+        <div class="h-64 bg-yellow-200 flex items-center justify-center rounded shadow">
+          Publicité
+        </div>
+      </div>
+
+    </section>
 
     <!-- Vitrine -->
     <Vitrine />
+
   </div>
 </template>
 
@@ -19,17 +35,27 @@ export default {
   components: { SliderProducts, Vitrine },
   setup() {
     const produits = ref([]);
+    const produitsPromos = ref([]);
 
     const fetchProduits = async () => {
       const snapshot = await getDocs(collection(db, "products"));
       const loaded = [];
-      snapshot.forEach((doc) => loaded.push({ id: doc.id, ...doc.data() }));
+      snapshot.forEach((doc) => {
+        const produit = { id: doc.id, ...doc.data() };
+        loaded.push(produit);
+        // Sélection des promos
+        if (produit.promo) produitsPromos.value.push(produit);
+      });
       produits.value = loaded;
     };
 
     onMounted(() => fetchProduits());
 
-    return { produits };
+    return { produits, produitsPromos };
   },
 };
 </script>
+
+<style scoped>
+/* Les styles sont gérés par Tailwind, rien à ajouter */
+</style>
