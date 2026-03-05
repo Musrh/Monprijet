30f9f630c64d226f63afcfa09e05effc11f16e3e
Musrh <musplay2023@gmail.com>
2026-03-05 21:12:42 +0000

    Update Home.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,23 @@
   <div class="px-4">
 
     <!-- Section Slider + Pub -->
-    <section class="flex flex-col md:flex-row gap-4 my-8">
+    <section class="flex flex-col md:flex-row my-8">
 
-      <!-- Slider des produits promos -->
-      <div class="md:w-3/4 w-full">
+      <!-- Slider produits -->
+      <div class="md:w-2/3 w-full">
         <SliderProducts :produits="produitsPromos" />
       </div>
 
-      <!-- Pub à côté du slider -->
-      <div class="md:w-1/4 w-full">
-        <div class="h-64 bg-yellow-200 flex items-center justify-center rounded shadow">
+      <!-- Publicité -->
+      <div class="md:w-1/3 w-full flex">
+        <div class="w-full min-h-[320px] bg-yellow-200 flex items-center justify-center shadow">
           Publicité
         </div>
       </div>
 
     </section>
 
-    <!-- Vitrine -->
+    <!-- Vitrine produits -->
     <Vitrine />
 
   </div>
@@ -33,24 +33,48 @@ import Vitrine from "../components/Vitrine.vue";
 
 export default {
   components: { SliderProducts, Vitrine },
+
   setup() {
     const produits = ref([]);
     const produitsPromos = ref([]);
 
     const fetchProduits = async () => {
       const snapshot = await getDocs(collection(db, "products"));
+
       const loaded = [];
+
       snapshot.forEach((doc) => {
         const produit = { id: doc.id, ...doc.data() };
+
         loaded.push(produit);
-        if (produit.promo) produitsPromos.value.push(produit);
+
+        if (produit.promo) {
+          produitsPromos.value.push(produit);
+        }
       });
+
       produits.value = loaded;
     };
 
     onMounted(() => fetchProduits());
 
-    return { produits, produitsPromos };
-  },
+    return {
+      produits,
+      produitsPromos
+    };
+  }
 };
 </script>
+
+<style scoped>
+
+/* Ajustement mobile */
+@media (max-width: 768px) {
+
+  section {
+    flex-direction: column;
+  }
+
+}
+
+</style>
