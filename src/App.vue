1c934c247f62c5e107dd99c0645b8194f4abefa5
Musrh <musplay2023@gmail.com>
2026-03-05 20:00:36 +0000

    Update App.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ import { ref, onMounted, computed } from "vue";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase";
 import SliderProducts from "./views/SliderProducts.vue";
-import Vitrine from "../components/Vitrine.vue";
+import Vitrine from "./components/Vitrine.vue";
 
 export default {
   components: { SliderProducts, Vitrine },
