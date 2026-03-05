520833a9de6a721e95b272353fb68646152bb700
Musrh <musplay2023@gmail.com>
2026-03-05 03:42:03 +0000

    Update Vitrine.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

--- src/components/Vitrine.vue
+++ src/components/Vitrine.vue
@@ -18,8 +18,94 @@
 
         <!-- 🔹 Prix et promo -->
         <div class="text-left">
-          <template v-if="p.promo">
-            <!-- Ancien prix -->
+          <span v-if="p.promo">
             <span class="text-gray-400 line-through mr-2">
               {{ p.prix }} €
-            </span
+            </span>
+            <span class="text-green-600 font-bold">
+              {{ calculatePromoPrice(p.prix) }} €
+            </span>
+          </span>
+          <span v-else class="text-green-600 font-bold">
+            {{ p.prix }} €
+          </span>
+        </div>
+
+        <!-- Bouton Ajouter au panier -->
+        <button
+          @click="ajouterAuPanier(p)"
+          class="mt-2 bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700"
+        >
+          Ajouter au panier
+        </button>
+
+        <!-- Étiquette promo -->
+        <span
+          v-if="p.promo"
+          class="absolute top-2 right-2 bg-red-500 text-white px-2 py-1 rounded"
+        >
+          PROMO
+        </span>
+      </div>
+    </div>
+
+    <div v-else class="text-gray-500 text-center">Aucun produit à afficher.</div>
+  </section>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import { collection, getDocs } from "firebase/firestore";
+import { useStore } from "vuex";
+import { db } from "../firebase";
+
+export default {
+  name: "Vitrine",
+  setup() {
+    const store = useStore();
+    const produits = ref([]);
+
+    const ajouterAuPanier = (produit) => {
+      store.dispatch("addToCart", {
+        ...produit,
+        image: produit.images,
+        quantity: 1
+      });
+      alert(`Produit "${produit.nom}" ajouté au panier !`);
+    };
+
+    const fetchProduits = async () => {
+      const snapshot = await getDocs(collection(db, "products"));
+      snapshot.forEach((doc) => {
+        produits.value.push({ id: doc.id, ...doc.data() });
+      });
+    };
+
+    const calculatePromoPrice = (prix) => {
+      return Math.round(prix * 0.5);
+    };
+
+    onMounted(() => {
+      fetchProduits();
+    });
+
+    return {
+      produits,
+      ajouterAuPanier,
+      calculatePromoPrice
+    };
+  }
+};
+</script>
+
+<style scoped>
+.vitrine img {
+  display: block;
+  width: 100%;
+  object-fit: cover;
+}
+
+.vitrine button {
+  transition: background-color 0.2s;
+}
+</style>
