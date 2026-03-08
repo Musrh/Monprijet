90d6953bd703f486aa2319f5d49beb727ede39ac
Musrh <musplay2023@gmail.com>
2026-03-08 02:22:26 +0000

    Update HeaderSearch.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -5,7 +5,7 @@
     <div class="flex items-center justify-between px-4 py-3 max-w-7xl mx-auto">
       <div class="flex items-center gap-3">
         <img src="../assets/hero.png" alt="logo" class="h-10" />
-        <span class="font-bold text-lg">Mon Site</span>
+        <span class="font-bold text-lg">EasyShoping</span>
       </div>
 
       <!-- Panier -->
