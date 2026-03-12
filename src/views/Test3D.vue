35791d121d1e0a68ad574a0db5477c6dea1cdea8
Musrh <musplay2023@gmail.com>
2026-03-12 18:42:02 +0000

    Create Test3D.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

---
+++ src/views/Test3D.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="padding:20px">
+
+    <h2>Produit en 3D</h2>
+
+    <model-viewer
+      src="/models/astronaut.glb"
+      auto-rotate
+      camera-controls
+      style="width:100%; height:400px;">
+    </model-viewer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test3D"
+}
+</script>
