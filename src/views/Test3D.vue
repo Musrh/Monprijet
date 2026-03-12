b14591c36dabf55b841a9b40e626891f874e0a75
Musrh <musplay2023@gmail.com>
2026-03-12 23:59:32 +0000

    Update Test3D.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

--- src/views/Test3D.vue
+++ src/views/Test3D.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">🚀 Produit en 3D</h1>
+  <div class="p-4 max-w-3xl mx-auto text-center">
+    <h1 class="text-3xl font-bold mb-6">🚀 Produit 3D à tester</h1>
 
-    <!-- Affiche l’astronaute -->
     <Product3D :product="astronaut" />
   </div>
 </template>
 
 <script>
-import Product3D from "../components/Product3D.vue";
+import Product3D from "@/components/Product3D.vue";
 
 export default {
   components: { Product3D },
@@ -17,9 +16,10 @@ export default {
       astronaut: {
         nom: "Astronaute 3D",
         prix: 49.99,
+        image: "/images/astronaut.png",
         modelUrl: "/models/Astronaut.glb"
       }
     };
-  }
+  },
 };
 </script>
