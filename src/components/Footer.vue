18936ccc6ce087a6c38c380c38c758794093a8a8
Musrh <musplay2023@gmail.com>
2026-03-13 01:30:08 +0000

    Create Footer.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="bg-gray-100 text-gray-600 p-4 text-center mt-8">
+    © 2026 WellShoppings •
+    <a href="#/" class="hover:text-gray-700 underline">Accueil</a> •
+    <a href="#/shop" class="hover:text-gray-700 underline">Boutique</a> •
+    <a href="#/conditions" class="hover:text-gray-700 underline">Conditions de Vente</a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+/* Ajustements éventuels pour mobile ou desktop */
+</style>
