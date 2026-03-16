eeed92eea846a8befd75238c63277fb6a32d409c
Musrh <musplay2023@gmail.com>
2026-03-16 01:57:43 +0000

    Update Home.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import PrintfulProducts from "../components/PrintfulProducts.vue"
+import PrintfulProducts from "./PrintfulProducts.vue"
 import { useStore } from "vuex"
 
 export default{
