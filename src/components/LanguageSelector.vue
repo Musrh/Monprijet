5780fbe05de72c5670b73ccce7e1d6ad2057aea9
Musrh <musplay2023@gmail.com>
2026-03-24 17:35:50 +0100

    Create LanguageSelector.vue
    Signed-off-by: Musrh <musplay2023@gmail.com>

---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex gap-2">
+    <button 
+      v-for="lang in languages" 
+      :key="lang.code"
+      @click="changeLanguage(lang.code)"
+      :class="{'font-bold underline': currentLang === lang.code}"
+    >
+      {{ lang.label }}
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "LanguageSelector",
+  data() {
+    return {
+      languages: [
+        { code: "fr", label: "FR" },
+        { code: "en", label: "EN" },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters(["currentLanguage"]),
+    currentLang() {
+      return this.currentLanguage;
+    },
+  },
+  methods: {
+    ...mapActions(["setLanguage"]),
+    changeLanguage(lang) {
+      this.setLanguage(lang);
+    },
+  },
+};
+</script>
