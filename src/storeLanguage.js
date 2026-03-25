db4331b8de62fb1b8a2fbd563d48e0fa460d29ed
Musrh <musplay2023@gmail.com>
2026-03-25 02:14:27 +0100

    Update storeLanguage.js
    Signed-off-by: Musrh <musplay2023@gmail.com>

--- src/storeLanguage.js
+++ src/storeLanguage.js
@@ -1,19 +1,28 @@
 // storeLanguage.js
 export default {
-  namespaced: true, // 🔹 important pour éviter conflits
-  state: {
-    currentLanguage: "fr", // langue par défaut
-  },
+  namespaced: true,
+
+  state: () => ({
+    currentLanguage:
+      localStorage.getItem("lang") ||
+      (navigator.language && navigator.language.startsWith("fr")
+        ? "fr"
+        : "en"),
+  }),
+
   mutations: {
     SET_LANGUAGE(state, lang) {
       state.currentLanguage = lang;
+      localStorage.setItem("lang", lang); // sauvegarde
     },
   },
+
   actions: {
     changeLanguage({ commit }, lang) {
       commit("SET_LANGUAGE", lang);
     },
   },
+
   getters: {
     currentLanguage: (state) => state.currentLanguage,
   },
